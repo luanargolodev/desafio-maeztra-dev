dc616324ff3906c6b2be92d64d6505a820a914b3
Luan Argolo <ipsluan@icloud.com>
2023-03-28 14:17:49 -0300

    feat(header): finish header

--- layout/src/components/Header/index.tsx
+++ layout/src/components/Header/index.tsx
@@ -1,10 +1,29 @@
 import BenefitBar from '../BenefitBar'
-import { Container } from './styles'
+import { Container, Content, IconMenu, Link, Logo, Left, Right } from './styles'
+
+import logoMaeztra from '../../assets/logo-maeztra.png'
+import svgMenu from '../../assets/icon-menu.svg'
+import svgSearch from '../../assets/search.svg'
+import svgBag from '../../assets/bag.svg'
 
 function Header() {
   return (
     <Container>
       <BenefitBar />
+
+      <Content>
+        <Left>
+          <IconMenu src={svgMenu} alt="Menu de opções" />
+          <Link href="https://maeztra.com/" target="_blank">
+            <Logo src={logoMaeztra} alt="Logo Maeztra" />
+          </Link>
+        </Left>
+
+        <Right>
+          <IconMenu src={svgSearch} alt="Buscar" />
+          <IconMenu src={svgBag} alt="Buscar" />
+        </Right>
+      </Content>
     </Container>
   )
 }
--- layout/src/components/Header/styles.ts
+++ layout/src/components/Header/styles.ts
@@ -1,3 +1,42 @@
 import styled from 'styled-components'
 
-export const Container = styled.header``
+export const Container = styled.header`
+  width: 100%;
+  max-width: 1920px;
+  height: 112px;
+`
+
+export const Content = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  height: 88px;
+  gap: 80px;
+`
+
+export const IconMenu = styled.img`
+  width: 24px;
+  height: 24px;
+  cursor: pointer;
+`
+
+export const Link = styled.a`
+  text-decoration: none;
+`
+
+export const Logo = styled.img`
+  width: 110px;
+  height: 12px;
+`
+
+export const Left = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 19px;
+`
+
+export const Right = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 40px;
+`
