0765422818ecc76452fd77651b44d956401f1712
Luan Argolo <ipsluan@icloud.com>
2023-03-28 10:53:44 -0300

    feat: resolve question 2

--- resposta_questao_2.js
+++ resposta_questao_2.js
@@ -1,1 +1,17 @@
+function hasDuplicateNumber(array) {
+  let numberDuplicates = []
 
+  array.filter((value, index) => {
+    // verifica se o valor do elemento atual é igual ao de algum elemento posterior
+    if (
+      array.indexOf(value, index + 1) !== -1 &&
+      numberDuplicates.indexOf(value) === -1
+    ) {
+      numberDuplicates.push(value)
+    }
+  })
+
+  return numberDuplicates
+}
+
+console.log(hasDuplicateNumber([4, 5, 44, 98, 4, 5, 6, 7]))
