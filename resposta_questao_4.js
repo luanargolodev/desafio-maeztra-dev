62a3fcce5ce37e4c7ac969cf3a5887278d0da50a
Luan Argolo <ipsluan@icloud.com>
2023-03-28 12:42:51 -0300

    feat: resolve question 4

--- resposta_questao_4.js
+++ resposta_questao_4.js
@@ -1,1 +1,25 @@
+function yearsWorked(years) {
+  let start = []
 
+  years.forEach((yearWorked) => {
+    // Tratamento de erros para valores inválidos
+    if (yearWorked[0] <= 0 || yearWorked[0] > yearWorked[1]) return
+
+    // Adiciona o ano de início de trabalho no array
+    start.push(yearWorked[0])
+  })
+
+  // Retorna o ano com mais trabalhadores
+  let mostWorkedYear = Math.max(...start)
+
+  return `Ano com mais trabalhadores: ${mostWorkedYear}`
+}
+
+console.log(
+  yearsWorked([
+    [1960, 2005],
+    [1945, 2008],
+    [1938, 1999],
+    [1967, 2000],
+  ])
+)
