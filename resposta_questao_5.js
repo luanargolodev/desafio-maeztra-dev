1ee07198739364e4380076ec7a28642d91de24da
Luan Argolo <ipsluan@icloud.com>
2023-03-28 13:04:05 -0300

    feat: resolve question 5

--- resposta_questao_5.js
+++ resposta_questao_5.js
@@ -1,1 +1,16 @@
+function possibleOrganizationForPhotos(people) {
+  // Tratamento de erros para o parâmetro people
+  if (!Number.isInteger(people) || people < 0)
+    return 'O número de pessoas precisa ser um inteiro'
 
+  let result = 1
+
+  // Realizando a matemática do n fatorial e somando ao resultado
+  for (let i = 1; i <= people; i++) {
+    result *= i
+  }
+
+  return `Existem ${result} possibilidades de organização para ${people} pessoas`
+}
+
+console.log(possibleOrganizationForPhotos(6))
