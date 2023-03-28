9cd83b61228979e52a58b49c48de7d012328c5cd
Luan Argolo <ipsluan@icloud.com>
2023-03-28 11:49:17 -0300

    feat: resolve question 3

---
+++ resposta_questao_3.js
@@ -0,0 +1,29 @@
+function isSequencyValid(sequence) {
+  const matchingBrackets = {
+    '(': ')',
+    '[': ']',
+    '{': '}',
+  }
+  const caracteres = []
+
+  // Percorre a sequência de caracteres
+  for (const char of sequence) {
+    // Se encontrar um caractere de abertura, adiciona ao stack
+    if (matchingBrackets[char]) {
+      caracteres.push(char)
+    }
+
+    // Se o caractere for um dos caracteres de fechamento, verifica se o último caractere do stack é o caractere de abertura correspondente
+    else if (char === ')' || char === ']' || char === '}') {
+      if (matchingBrackets[caracteres.pop()] !== char) {
+        return 'Não é uma sequência válida'
+      }
+    }
+  }
+
+  return caracteres.length === 0
+    ? 'Sequência válida'
+    : 'Não é uma sequência válida'
+}
+
+console.log(isSequencyValid('{[()(){}[]]{}}'))
