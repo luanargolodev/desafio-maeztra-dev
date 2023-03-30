bd0c614e04b487915a775543b2c02e66609a0657
Luan Argolo <ipsluan@icloud.com>
2023-03-29 23:05:50 -0300

    feat(form): validate email

--- layout/src/components/Form/index.tsx
+++ layout/src/components/Form/index.tsx
@@ -1,15 +1,28 @@
+import { useState } from 'react'
 import { FormEmail, Input, Button } from './styles'
+import { validateEmail } from '../../utils/validateEmail'
 
 const Form = () => {
+  const [email, setEmail] = useState('')
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault()
+
+    if (!validateEmail(email)) {
+      alert('E-mail inválido!')
+      return
+    }
 
     alert('Formulário enviado com sucesso!')
   }
 
   return (
     <FormEmail onSubmit={handleSubmit}>
-      <Input type="email" placeholder="Digite seu e-mail" />
+      <Input
+        type="email"
+        placeholder="Digite seu e-mail"
+        value={email}
+        onChange={(event) => setEmail(event.target.value)}
+      />
       <Button type="submit">Enviar</Button>
     </FormEmail>
   )
